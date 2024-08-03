2f7c4373c630d0e7d4031edc28ab1fb3012c07f7
AmalRajeev <amalrajeev2013@gmail.com>
2024-08-03 14:57:14 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: [
-      '@mui/material',
-      '@mui/icons-material',
-      '@mui/x-date-pickers',
-      'date-fns'
-    ]
-  }
-});
+})
+
+
