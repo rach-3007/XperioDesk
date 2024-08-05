a2d3d9a631b233d7733beb60b2a0c657d1ee494b
Keernage <sethumanohar@gmail.com>
2024-08-05 10:26:48 +0530

    UserViewAllBookingsInitialCommit

--- src/Pages/UserViewAllBookingsPage.tsx
+++ src/Pages/UserViewAllBookingsPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import UserViewAllBookings from '../Components/UserViewAllBookings/UserViewAllBookings'
 
 const UserViewAllBookingsPage = () => {
   return (
     <div>
-      
+      <UserViewAllBookings></UserViewAllBookings>
     </div>
   )
 }
