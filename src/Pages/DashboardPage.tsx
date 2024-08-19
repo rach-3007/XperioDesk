ea7b5d08bed492599400b45c15407e5781298f37
rachelrajan_exp <rachel.rajan@experionglobal.com>
2024-08-19 09:18:37 +0530

    Dashboard Updated

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -4,8 +4,7 @@ import Dashboard from '../Components/Dashboard/Dashboard'
 const DashboardPage = () => {
   return (
     <div>
-      <Dashboard></Dashboard>
-    </div>
+<Dashboard/>    </div>
   )
 }
 
