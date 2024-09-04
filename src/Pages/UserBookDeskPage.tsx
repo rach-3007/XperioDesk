eae0b327570f7bb74bf92acf5e12b16b3baa3a5a
rachelrajan_exp <rachel.rajan@experionglobal.com>
2024-09-04 09:25:12 +0530

    user Book Desk

---
+++ src/Pages/UserBookDeskPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import UserBookDesk from "../Components/UserBookDesk/UserBookDesk";
+
+
+
+const UserBookDeskPage = () => {
+  return (
+    <div>
+      <UserBookDesk></UserBookDesk>
+    </div>
+  )
+}
+
+export default UserBookDeskPage
