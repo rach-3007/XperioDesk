6e2b26d7a142dcc790924887195910fc363cdba6
Keernage <sethumanohar@gmail.com>
2024-09-04 09:41:19 +0530

    ModalSnackbar and added userbookdeskpage

---
+++ src/Pages/UserBookDeskPage.tsx
@@ -0,0 +1,13 @@
+import UserBookDesk from "../Components/UserBookDesk/UserBookDesk";
+
+import React from 'react'
+
+const UserBookDeskPage = () => {
+  return (
+    <div>
+      <UserBookDesk></UserBookDesk>
+    </div>
+  )
+}
+
+export default UserBookDeskPage
